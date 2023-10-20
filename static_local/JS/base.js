e30e23b0345ec848aa70888434a2b560c521f3bb
Arata1202~ <aratadayoon@yahoo.co.jp>
2023-10-20 16:40:02 +0900

    jQuery導入

--- static_local/JS/base.js
+++ static_local/JS/base.js
@@ -1,8 +1,14 @@
 'use strict'
 
+//途中までスクロール
 function ScrollWindow(elem) {
     var element = document.getElementById(elem);
     var rect = element.getBoundingClientRect();
     var elemtop = rect.top + window.pageYOffset;
     document.documentElement.scrollTop = elemtop;
+}
+
+//一番上までスクロール
+function scrollToTop() {
+    scrollTo(0, 0);
 }
