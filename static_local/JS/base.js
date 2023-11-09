cc0b11d6b67fc71b9a27d58a4ea0d015ede61071
Arata <142312848+Arata1202@users.noreply.github.com>
2023-11-09 21:20:31 +0900

    Update base.js

--- static_local/JS/base.js
+++ static_local/JS/base.js
@@ -1,4 +1,42 @@
+//　　　　　ハンバーガーメニュー
 'use strict'
+const btn = document.querySelector('.btn');
+const block = document.querySelector('.block');
+btn.addEventListener('click', () => {
+    btn.classList.toggle('active');
+    block.classList.toggle('active');
+});
+
+//　　　　　内部リンク　ハンバーガーオフ
+const link_top = document.querySelector('.link_top');
+link_top.addEventListener('click', () => {
+    btn.classList.remove('active');
+    block.classList.remove('active');
+});
+
+const link_home = document.querySelector('.link_home');
+link_home.addEventListener('click', () => {
+    btn.classList.remove('active');
+    block.classList.remove('active');
+});
+
+const link_about = document.querySelector('.link_about');
+link_about.addEventListener('click', () => {
+    btn.classList.remove('active');
+    block.classList.remove('active');
+});
+
+const link_service = document.querySelector('.link_service');
+link_service.addEventListener('click', () => {
+    btn.classList.remove('active');
+    block.classList.remove('active');
+});
+
+const link_skills = document.querySelector('.link_skills');
+link_skills.addEventListener('click', () => {
+    btn.classList.remove('active');
+    block.classList.remove('active');
+});
 
 //途中までスクロール
 function ScrollWindow(elem) {
